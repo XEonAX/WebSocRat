86df9284877861ea9f5fc450ea9e704ff47ff42a
XEonAX <sv.xeon@gmail.com>
2017-05-15 21:15:40 +0530

    servername

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const ip = env.NODE_IP || "0.0.0.0";
 const app = express();
 const crypto = require("crypto");
 const faker = require('faker');
+const servername = faker.company.catchPhrase();
 app.use(express.static('static'))
 // app.use('/static', express.static('static'))
 // app.use(express.static('bootstrap-gh-pages'));
@@ -41,7 +42,7 @@ wss.on("connection", function (ws) {
   }));
   ws.send(JSON.stringify({
     type: "message",
-    message: "Welcome " + username + "!",
+    message: "Welcome " + username + " to " + servername,
     timestamp: Date.now().toString(),
     fromId: 0,
     sender: "AEonAX",
